0281daa0b1153ee3d0b6c3fe2048a1faa5aa7e06
minhajul islam <mm01882390860@gmail.com>
2025-05-10 18:37:21 +0600

    add number to string convertion example with tostring()

---
+++ script/number-method.js
@@ -0,0 +1,18 @@
+let num = 456;
+
+let str = num.toString()
+
+let num1 = str + " is a number";
+
+console.log(num1);
+
+
+{
+    let number = 321;
+
+    let num = number.toString();
+
+    let totalString = num + " is a number";
+
+    console.log(totalString);
+}
