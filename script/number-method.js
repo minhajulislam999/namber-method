0ef6831e87e94f8b4db192603716405ecb64f616
minhajul islam <mm01882390860@gmail.com>
2025-05-10 19:32:26 +0600

    add practice: sum of tow numbers and convert to string with message

--- script/number-method.js
+++ script/number-method.js
@@ -26,3 +26,16 @@ console.log(num1);
 
     console.log(num);
 }
+
+{
+    let x = 100;
+    let a = 50;
+
+    let total = x + a;
+
+    let str = total.toString();
+
+    let num = str + " is the total amount";
+
+    console.log(num);
+}
